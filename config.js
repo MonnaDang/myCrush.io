5d44e0a222f4b49d448ef4d97b40da03edce1f4a
MonnaDang <chuongmap0979511903@gmail.com>
2022-07-29 22:33:37 +0700

    upload source code

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Cô nương à!!',
+    introDesc: `tui chỉ muốn tôi chỉ muốn nói một điều thôi 👉👈`,
+    btnIntro: 'hihi',
+    title: 'Nếu nói về nhan sắc thì 😙',
+    desc: 'Cậu xứng đáng 10 điểm xinh đẹp, 9 điểm dễ thương, 8 chuyện với tui một tí được không 👉👈',
+    btnYes: 'Tới mai luôn cũng được <33',
+    btnNo: 'Mơ đi, a zai à :3',
+    question:'Nói 1 điều mà bà thấy ấn tượng về tui đi <3',
+    btnReply: 'Gửi cho tui<3',
+    reply: 'Siêu đẹp trai nhất vũ trụ này luônnnn <33333333',
+    mess: 'Tui biết mà 🥰. Cảm ơn bà nhiều nhiều 😘😘',
+    messDesc: 'Hôm nào rảnh mình đi xem phim nhé !!!',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://m.me/chuongheo123' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
